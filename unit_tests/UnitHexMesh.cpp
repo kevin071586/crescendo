87e7f4a71d7bb0784f23d9a97375d586ff019ecb
Kevin Manktelow <kevin071586@yahoo.com>
2021-01-02 14:42:07 -0800

    add unit test to check for existence of coordinate field

--- unit_tests/UnitHexMesh.cpp
+++ unit_tests/UnitHexMesh.cpp
@@ -13,3 +13,16 @@ TEST(HexMesh, ElemId) {
     EXPECT_EQ( hf.elem_id(1,0,0), 2u );
     EXPECT_EQ( hf.elem_id(2,0,0), 3u );
 }
+
+TEST(HexMesh, HasCoordinateField) {
+    // Test 3x1x1 HexFixture structure
+    const unsigned NX = 3;
+    const unsigned NY = 1;
+    const unsigned NZ = 1;
+    stk::mesh::fixtures::HexFixture hf(MPI_COMM_WORLD,NX,NY,NZ);
+    hf.m_meta.commit();
+    hf.generate_mesh();
+
+    auto coordField = hf.m_meta.get_field(stk::topology::NODE_RANK, "coordinates");
+    EXPECT_FALSE(coordField == nullptr);
+}
